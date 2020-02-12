80a6a5f82a78950c6c680d627265c0f19de43390
Josh Wulf <josh@magikcraft.io>
2020-02-13 09:52:43 +1000

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const max = core.getInput('max') || '10'
+    core.setOutput(
+      'number',
+      (Math.round(Math.random() * parseInt(max, 10)) + 1).toString()
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
